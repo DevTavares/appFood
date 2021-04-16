7848c58a493dab776223fbcfd8985faa78d7e606
Thiago Paravati <thiago.paravati@hotmail.com>
2021-04-15 23:36:46 -0300

    alteracao na parte do feed e adicionado rascunho de search

--- src/app/main-home/feed/feed.page.ts
+++ src/app/main-home/feed/feed.page.ts
@@ -6,10 +6,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./feed.page.scss'],
 })
 export class FeedPage implements OnInit {
+  public posts = [
+    {
+      user: 'Roberto Pereira',
+      id: 55,
+      liked: true,
+      postName: 'Coxinha de Frango',
+      time: 30,
+      star: 4.3,
+      saved : false,
+    },
+    {
+      user: 'Abner Almeida',
+      id: 80,
+      liked: false,
+      postName: 'Bolo de Chocolate',
+      time: 60,
+      star: 4.7,
+      saved : false,
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  public toggleLike(post) {
+    post.liked = !post.liked;
   }
-
+  public toggleSave(post) {
+    post.saved = !post.saved;
+  }
 }
